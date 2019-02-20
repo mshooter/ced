46e4037aad7cab087f8db36ba71c342a95391171
mshooter <moira.shooter@hotmail.com>
2019-02-20 17:08:56 +0000

    temporary Gradients, need to remove the maxValue and minValue

--- ced_cpu/include/Gradients.hpp
+++ ced_cpu/include/Gradients.hpp
@@ -2,9 +2,12 @@
 #ifndef __GADIENTS_H_INCLUDED__
 #define __GADIENTS_H_INCLUDED__
 
+#include <vector>
+
 namespace ced 
 {
-    std::vector<float> calculateGradients(int _height, int _width, int _pixelData);
+    void calculateGradients(int& _height, int& _width, std::vector<float>& _pixelDatam, std::vector<float>& _orientations,
+                            float _minValue = 0.0f, float _maxValue = 1.0f);
 }
 #endif //__GAUSSIANFILTER_H_INCLUDED__
 
--- ced_cpu/src/Gradients.cpp
+++ ced_cpu/src/Gradients.cpp
@@ -1,8 +1,9 @@
 #include "Gradients.hpp"
-
+#include <cmath>
+#include <iostream>
 namespace ced
 {
-    void calculateGradients(int& _height, int& _width, std::vector<float> _pixelData, std::vector<float>& _mag, std::vector<float>& _orientation)
+    void calculateGradients(int& _height, int& _width, std::vector<float>& _pixelData, std::vector<float>& _orientations, float _minValue, float _maxValue)
     {
         // sobel edge detector filters 
         std::vector<float> kernelX = {-1, 0, 1,
@@ -10,12 +11,41 @@ namespace ced
                                     -1, 0, 1};
         std::vector<float> kernelY = {-1, -2, -1,
                                      0,  0,  0,
-                                    -1, -2, -1};
+                                    1, 2, 1};
         _height -= 2; 
-        _width -= 2;
-        // iterate through image 
-        // iterate through kernels
-        // calculate magnitude 
-        // calculate orientation
+        int nwidth = _width - 2;
+        _orientations.resize(_height * nwidth);
+        for(int i=0; i < _height; ++i)
+        {
+            for(int j=0; j < nwidth; ++j)
+              {
+                float Gx = 0.0f;
+                float Gy = 0.0f;
+                 for(int h=i; h < i + 3; ++h)
+                 {
+                     for(int w=j; w < j + 3; ++w)
+                     {
+
+                          int ibase = (w+h*_width) * 3;
+                          int fbase = ((h-i) + (w-j) * 3);
+                          Gx +=  (_pixelData[ibase+0] + _pixelData[ibase+1] + _pixelData[ibase+2]) * kernelX[fbase];
+                          Gy +=  (_pixelData[ibase+0] + _pixelData[ibase+1] + _pixelData[ibase+2]) * kernelY[fbase];
+                     }
+                 }
+                int base = (j+i*nwidth)* 3;
+                float magnitude = std::abs(Gx) + std::abs(Gy); 
+                float theta = std::atan(Gy/Gx) * 180/3.14f;
+                //std::cout<<theta<<std::endl;
+                _pixelData[base+0] = magnitude > _maxValue ? 1.0f : magnitude;
+                _pixelData[base+1] = magnitude > _maxValue ? 1.0f : magnitude;
+                _pixelData[base+2] = magnitude > _maxValue ? 1.0f : magnitude;
+
+                _pixelData[base+0] = magnitude < _minValue ? 0.0f : magnitude;
+                _pixelData[base+1] = magnitude < _minValue ? 0.0f : magnitude;
+                _pixelData[base+2] = magnitude < _minValue ? 0.0f : magnitude;
+                _orientations.push_back(theta);
+              }
+          }
+        _width = std::move(nwidth);
     }
 }

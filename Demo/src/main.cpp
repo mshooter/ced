9b28fb173e38d619de017e31a5e649a55d7baaac
mshooter <moira.shooter@hotmail.com>
2019-03-04 12:54:40 +0000

    drawing lines test

--- Demo/src/main.cpp
+++ Demo/src/main.cpp
@@ -3,6 +3,11 @@
 #include "Image.hpp"
 #include "NonMaximumSupression.hpp"
 #include "Hysterysis.hpp"
+#include "Point.hpp"
+#include "DrawLine.hpp"
+#include "GenerateRandomPoints.hpp"
+#include "SortPoints.hpp"
+#include "SplitVector.hpp"
 
 int main()
 {
@@ -24,36 +29,74 @@ int main()
         const char *finalout    = "/home/s4928793/Desktop/Cat/finalcat.jpg";
     #endif
     ced::cpu::Image img(filename);
-    // create filter gaussian blur
-    const int gDimension = 5;
-    std::vector<float> gfilter = ced::cpu::gaussianFilter(gDimension, 1.4f); 
-    // convert to gray scale
-    img.convertToGrayscale();
-    //img.saveImage(outgray);
-    // apply gaussian filter
-    img.applyFilter(gfilter, gDimension);
-    //img.saveImage(outgaussian);
-    // sobel operator to get magnitude and orientation
-    int height = img.getHeight();
-    int width = img.getWidth();
-    std::vector<float> orientation;
-    std::vector<float> magnitude = img.getPixelData();
-    // need to work on this  
-    ced::cpu::calculateGradients(height,
-                          width, 
-                          magnitude,
-                          orientation
-                          );
-    // nonmaximum supression    
-    ced::cpu::nonMaximumSupression(height, width, orientation, magnitude);
-    img.setHeight(height);
-    img.setWidth(width);
+    // get random points
+    std::vector<ced::cpu::Point> coord = ced::cpu::generateRandomPoints(100, img.getHeight(), img.getWidth());
+    // sort the points
+    ced::cpu::quickSort(coord, 0, coord.size()-1);
+    // img data 
+    std::vector<float> imgData(img.getHeight() * img.getWidth() * 3, 0.0f);
+    // split the vector 
+    std::vector<std::vector<ced::cpu::Point>> fvec;
+    ced::cpu::splitVector(fvec, coord);
+    // draw the points
+    // draw the lines now
+    for(auto t : fvec)
+    {   
+        for(auto x : t)
+        {
+            int xx = x.getX();
+            int xy = x.getY();
+            for(int i = 0; i < img.getHeight(); ++i)
+            {
+                for(int j=0; j < img.getWidth(); ++j)
+                {
+                    if(xy == i && xx == j)
+                    {
+                        imgData[(j+i*img.getWidth()) * 3 + 0] = 1;
+                        imgData[(j+i*img.getWidth()) * 3 + 1] = 1;
+                        imgData[(j+i*img.getWidth()) * 3 + 2] = 1;
+                    }
+                }
+            }
+        }
+
+    }
+
+    // draw the lines
+    img.setPixelData(imgData);
+    img.saveImage(outgradient);
+    //// create filter gaussian blur
+    //const int gDimension = 5;
+    //std::vector<float> gfilter = ced::cpu::gaussianFilter(gDimension, 1.4f); 
+    //// convert to gray scale
+    //img.convertToGrayscale();
+    ////img.saveImage(outgray);
+    //// apply gaussian filter
+    //img.applyFilter(gfilter, gDimension);
+    ////img.saveImage(outgaussian);
+    //// sobel operator to get magnitude and orientation
+    //int height = img.getHeight();
+    //int width = img.getWidth();
+    //std::vector<float> orientation;
+    //std::vector<float> magnitude = img.getPixelData();
+    //// need to work on this  
+    //ced::cpu::calculateGradients(height,
+    //                      width, 
+    //                      magnitude,
+    //                      orientation
+    //                      );
+    //// nonmaximum supression    
+    //ced::cpu::nonMaximumSupression(height, width, orientation, magnitude);
+    //img.setHeight(height);
+    //img.setWidth(width);
     //img.setPixelData(magnitude);
     //img.saveImage(outgradient);
     // final image
-    ced::cpu::hysterysis(magnitude, height, width, 0.2f, 0.3f);
-    img.setPixelData(magnitude);
-    img.saveImage(finalout);
+    //ced::cpu::hysterysis(magnitude, height, width, 0.2f, 0.3f);
+    //img.setPixelData(magnitude);
+    //img.saveImage(finalout);
+    
+
     
     return 0;
 }

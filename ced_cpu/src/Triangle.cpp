e586ed95c7ad3b2573b07f9400f9e6add1e90994
mshooter <moira.shooter@hotmail.com>
2019-03-13 16:59:09 +0000

    implemented the Triangle class

---
+++ ced_cpu/src/Triangle.cpp
@@ -0,0 +1,79 @@
+#include "Triangle.hpp"
+
+namespace ced
+{
+    namespace cpu
+    {
+        Triangle::Triangle()
+        {
+            m_vertices.reserve(3);
+            m_neighbourTriangles.reserve(3);
+        }
+        //-------------------------------------
+        Triangle::Triangle(Point* p1, Point* p2, Point* p3)
+        {
+            m_vertices.reserve(3);
+            m_vertices.push_back(p1);
+            m_vertices.push_back(p2);
+            m_vertices.push_back(p3);
+            m_neighbourTriangles.reserve(3);
+        }
+        //-------------------------------------
+        Triangle::Triangle(std::vector<Point*> _points)
+        {
+            m_vertices.reserve(3);
+            m_vertices = std::move(_points);
+            m_neighbourTriangles.reserve(3);
+        }
+        //-------------------------------------
+        Triangle::Triangle(std::vector<Triangle*> _triangles)
+        {
+            m_vertices.reserve(3);
+            m_neighbourTriangles.reserve(3);
+            m_neighbourTriangles = std::move(_triangles);
+        }
+        //-------------------------------------
+        Triangle::Triangle(std::vector<Point*> _points, std::vector<Triangle*> _triangles)
+        {
+            m_vertices.reserve(3);
+            m_vertices = std::move(_points);
+            m_neighbourTriangles.reserve(3);
+            m_neighbourTriangles = std::move(_triangles);
+        }
+        //-------------------------------------
+        Triangle::~Triangle()
+        {
+            for(auto x : m_vertices)
+            {
+                delete x;
+            }
+            m_vertices.clear();
+
+            for(auto x : m_neighbourTriangles)
+            {
+                delete x;
+            }
+            m_neighbourTriangles.clear();
+        }
+        //-------------------------------------
+        void Triangle::addVertex(Point* _vertex)
+        {
+            m_vertices.push_back(_vertex);
+        }
+        //-------------------------------------
+        void Triangle::addTriangle(Triangle* _triangle)
+        {
+            m_neighbourTriangles.push_back(_triangle);
+        }
+        //-------------------------------------
+        std::vector<Point*> Triangle::getVertices()
+        {
+            return m_vertices;
+        }
+        //-------------------------------------
+        std::vector<Triangle*> Triangle::getNeighbourTriangles()
+        {
+            return m_neighbourTriangles;
+        }
+    }
+}

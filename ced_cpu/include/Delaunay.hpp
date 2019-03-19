fad91e82db3f431d2d4087a2f0058f3bdc2c6bae
mshooter <moira.shooter@hotmail.com>
2019-03-19 19:19:59 +0000

    implementing the triangulation function, index of baseEdge is wrong

--- ced_cpu/include/Delaunay.hpp
+++ ced_cpu/include/Delaunay.hpp
@@ -5,26 +5,50 @@
 #include <algorithm>
 #include "Point.hpp"
 
+// TODO: create class DelaunayTri because it is a big class and you do not want all the functions public
+// TODO : the hull should be a list
 namespace ced
 {
     namespace cpu
     {
-        //--------------------------------------------------------------------------
-        /// @build : checks if the reverse of an edge is in the hull
-        /// @param[_in] _edge : the edge to check if its reverse is in the hull
-        /// @param[_in] _hull : the list of hull
-        ///--------------------------------------------------------------------------
-        template <typename T>
-        bool isReverseEdgeInHull(T _edge, std::vector<T> _hull);
-        //--------------------------------------------------------------------------
+
+        //  --------------------------------------------------------------------------
         /// @build : checks if the third point is left from the first and second point
         /// the first point is mostly the startpoint of the edge and idem for last point ( end point)
         /// @param[_in] _p1 : first point (start point of edge)
         /// @param[_in] _p2 : second point (end point of edge)
         /// @param[_in] _p3 : third point (point to be added to form a triangle)
-        //--------------------------------------------------------------------------
+        //  --------------------------------------------------------------------------
         bool isLeft(Point _p1, Point _p2, Point _p3);
-
+        //  --------------------------------------------------------------------------
+        /// @build : checks if the reverse of an edge is in the hull
+        /// @param[_in] _edge : the edge to check if its reverse is in the hull
+        /// @param[_in] _hull : the list of hull
+        //  --------------------------------------------------------------------------
+        template <typename T>
+        bool isReverseEdgeInHull(T _edge, std::vector<T> _hull);
+        //  --------------------------------------------------------------------------
+        /// @build : inserting edge infront 
+        /// @param[_in] _element : element to insert in the hull
+        /// @param[_in] _bei : base edge index 
+        //  --------------------------------------------------------------------------
+        template <typename T>
+        void insertBeforeElement(T _element, typename std::vector<T>::iterator _bei, std::vector<T>& _hull);
+        //  --------------------------------------------------------------------------
+        /// @build : inserting edge  after element
+        /// @param[_in] _element : element to insert in the hull
+        /// @param[_in] _bei : base edge index 
+        //  --------------------------------------------------------------------------
+        template <typename T>
+        void insertAfterElement(T _element, typename std::vector<T>::iterator _bei, std::vector<T>& _hull);
+        //  --------------------------------------------------------------------------
+        /// @build : need a delaunay function that iterates over the rest of the points
+        /// @param[_in] _points : the points are already sorted
+        //  --------------------------------------------------------------------------
+        void triangulate(std::vector<Point> _points);
+        //  --------------------------------------------------------------------------
+        /// @build : inserting the delaunay template functions
+        //  --------------------------------------------------------------------------
         #include "Delaunay.inl"
     }
 }
--- ced_cpu/include/Delaunay.inl
+++ ced_cpu/include/Delaunay.inl
@@ -3,6 +3,7 @@ bool isReverseEdgeInHull(T _edge, std::vector<T> _hull)
 {
     // swap the elements in the edge
     std::swap(_edge.startPoint, _edge.endPoint);  
+    // i dont think you need to iterate over because you dont need to search, but this is an option
     for(auto edge : _hull)
     {
         if(_edge == edge)
@@ -12,3 +13,31 @@ bool isReverseEdgeInHull(T _edge, std::vector<T> _hull)
     }
     return false;
 }
+//--------------------------------------------------------------------------
+template <typename T>
+void insertBeforeElement(T _element, typename std::vector<T>::iterator _bei, std::vector<T>& _hull)
+{
+    // bei is base index
+    if(_bei != _hull.begin())
+    {
+        _hull.insert(_bei - 1, _element);
+    }
+    else
+    {
+        _hull.insert(_bei, _element);
+    }
+}
+//--------------------------------------------------------------------------
+template <typename T>
+void insertAfterElement(T _element, typename std::vector<T>::iterator _bei, std::vector<T>& _hull)
+{
+    if(_bei != _hull.end())
+    {
+        _hull.insert(_bei + 1, _element);
+    }
+    else
+    {
+        _hull.insert(_bei, _element);
+    }
+}
+
